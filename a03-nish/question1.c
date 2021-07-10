6ef3f5688f980327512b68357dbd2d251e34ece8
Nish <nishtewari2001@gmail.com>
2021-07-10 00:49:08 -0400

    Finish Question 1

--- a03-nish/question1.c
+++ a03-nish/question1.c
@@ -13,11 +13,15 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<pthread.h>
+#include<semaphore.h>
 
 // Global Data variables.
 int a = 5, b = 7;
+sem_t lock;
+
 // Function that access the global data.
 void* inc_dec(void *arg) {
+	sem_wait(&lock);
 	printf("Read value of 'a' global variable is: %d\n", a);
 	printf("Read value of 'b' global variable is: %d\n", b);
 	sleep(1);
@@ -25,10 +29,12 @@ void* inc_dec(void *arg) {
 	b = b - 1;
 	printf("Updated value of 'a' variable is: %d\n", a);
 	printf("Updated value of 'b' variable is: %d\n", b);
+	sem_post(&lock);
 	return 0;
 }
 int main() {
 	// Creating the thread instances.
+	sem_init(&lock, 0,1);
 	pthread_t t1, t2, t3;
 	pthread_create(&t1, NULL, inc_dec, NULL);
 	pthread_create(&t2, NULL, inc_dec, NULL);
@@ -37,9 +43,13 @@ int main() {
 	pthread_join(t2, NULL);
 	pthread_join(t3, NULL);
 
+
 	//Destroying the threads.
-	pthread_exit(t1);
-	pthread_exit(t2);
-	pthread_exit(t3);
+	pthread_exit(&t1);
+	pthread_exit(&t2);
+	pthread_exit(&t3);
+
+
+	sem_destroy(&lock); //Destroy the lock
 	return 0;
 }
